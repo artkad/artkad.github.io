a6c3a8ae2aad7828b91d7b7b2fe29f015a9ed53e
artkad <53867789+artkad@users.noreply.github.com>
2020-04-12 14:37:09 +0500

    add server

---
+++ project-express/server/cart.js
@@ -0,0 +1,18 @@
+let add = (cart, req) => {
+	cart.contents.push(req.body);
+	return JSON.stringify(cart, null, 4);
+};
+let change = (cart, req) => {
+	let find = cart.contents.find(el => el.id_product === +req.params.id);
+	
+	find.quantity += req.body.quantity;
+	
+	if(find.quantity == 0){
+				cart.contents.splice(cart.contents.indexOf(find), 1);
+	}
+	return JSON.stringify(cart, null, 4);
+};
+module.exports = {
+	add,
+	change
+};
