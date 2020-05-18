069a2bb9785d2ce0040c3650199585d76fae3fb4
devart <kadkin.artem@gmail.com>
2020-05-18 11:33:48 +0500

    added dir starDB

---
+++ starBD/precache-manifest.07ba3f1eb8e9a678019292f36fd5c5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beae9d555fb69cc026e46992769f01b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e15eb4e5505901407b49",
+    "url": "/static/css/main.d0ed21b0.chunk.css"
+  },
+  {
+    "revision": "21afa947ddceb35117d3",
+    "url": "/static/js/2.950a81a4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.950a81a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15eb4e5505901407b49",
+    "url": "/static/js/main.91f479e8.chunk.js"
+  },
+  {
+    "revision": "0a9ed1ed035e91451a23",
+    "url": "/static/js/runtime-main.a407ddaa.js"
+  }
+]);
